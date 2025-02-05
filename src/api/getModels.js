999fd097a28d05995e7dc61b99327ffdea091676
klima3775 <113065590+klima3775@users.noreply.github.com>
2025-02-05 15:14:38 +0200

    refactor model fetching logic; improve error handling and localize UI text

--- src/api/getModels.js
+++ src/api/getModels.js
@@ -3,8 +3,10 @@ export async function getModels(makeId, year) {
     const response = await fetch(
       `https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMakeIdYear/makeId/${makeId}/modelyear/${year}?format=json`
     );
+    if (!response.ok) throw new Error("data loading error");
+    return response.json();
   } catch (error) {
     console.error(error);
-    return [];
+    return { Results: [] };
   }
 }
--- src/app/result/[makeId]/[year]/page.js
+++ src/app/result/[makeId]/[year]/page.js
@@ -1,47 +1,19 @@
-export async function generateStaticParams() {
-  const res = await fetch(
-    "https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/car?format=json"
-  );
-  const makes = await res.json();
-
-  const years = Array.from(
-    { length: new Date().getFullYear() - 2014 },
-    (_, i) => 2015 + i
-  );
-
-  return makes.Results.flatMap((make) =>
-    years.map((year) => ({
-      makeId: make.MakeId.toString(),
-      year: year.toString(),
-    }))
-  );
-}
-
-async function getData(makeId, year) {
-  const res = await fetch(
-    `https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMakeIdYear/makeId/${makeId}/modelyear/${year}?format=json`
-  );
-  if (!res.ok) throw new Error("Ошибка загрузки данных");
-  return res.json();
-}
+import { getModels } from "../../../../api/getModels";
 
 export default async function ResultsPage({ params }) {
-  const { makeId, year } = await params;
-  let data;
+  const { makeId, year } = params;
 
+  let data;
   try {
-    const response = await fetch(
-      `https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMakeIdYear/makeId/${makeId}/modelyear/${year}?format=json`
-    );
-    data = await response.json();
+    data = await getModels(makeId, year);
   } catch (error) {
-    console.error("Ошибка загрузки моделей:", error);
+    console.error("Error loading car models:", error);
     data = { Results: [] };
   }
 
   return (
     <div>
-      <h1 className="text-2xl font-bold mb-4">Модели автомобилей</h1>
+      <h1 className="text-2xl font-bold mb-4">Car models</h1>
       <ul>
         {data.Results.map((model) => (
           <li key={model.Model_ID}>{model.Model_Name}</li>
