3c41ac8c4f988a784a3922c73b424e7944828798
klima3775 <113065590+klima3775@users.noreply.github.com>
2025-02-05 16:20:15 +0200

    fix problem with key

--- src/app/result/[makeId]/[year]/page.js
+++ src/app/result/[makeId]/[year]/page.js
@@ -15,9 +15,9 @@ export default async function ResultsPage({ params }) {
     <div className="flex flex-col items-center justify-center min-h-screen p-4 bg-background">
       <h1 className="text-2xl font-bold mb-4 text-foreground">Car models</h1>
       <ul className="w-full max-w-md bg-white p-4 border border-gray-300 rounded shadow-md space-y-2">
-        {data.Results.map((model) => (
+        {data.Results.map((model, index) => (
           <li
-            key={model.Model_ID}
+            key={`${model.Model_ID}-${index}`}
             className="p-2 border-b last:border-none text-foreground"
           >
             {model.Model_Name}
